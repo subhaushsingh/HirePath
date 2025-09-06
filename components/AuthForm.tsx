16d993182491b7f1e2fdc7b8cd3ec86d16437b2c
Subh Aush Singh <subhaushsingh@gmail.com>
2025-09-06 16:10:37 +0530

    formfield for authentication

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -4,6 +4,7 @@ import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
 
+
 import { Button } from "@/components/ui/button"
 import {
   Form,
@@ -15,47 +16,44 @@ import {
   FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
+import Image from "next/image"
 
 const formSchema = z.object({
   username: z.string().min(2).max(50),
 })
 
 const AuthForm = () => {
-      const form = useForm<z.infer<typeof formSchema>>({
+    const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       username: "",
     },
   })
  
-  
+  // 2. Define a submit handler.
   function onSubmit(values: z.infer<typeof formSchema>) {
-    
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
     console.log(values)
   }
   return (
-    <div>
-      <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+    <div className="flex justify-center items-center min-h-screen">
+    <div className=" card-border lg:min-w-[566px]">
+      <div className="flex flex-col gap-6 card py-14 px-10">
+        <div className="flex flex-row justify-center gap-2">
+            <Image src="/logo.svg" alt="logo" height={32} width={38} />
+            <h2 className="text-primary-100">HirePath</h2>
+        </div>
+         <h3>Practice job interviews with AI</h3>
+      
+       <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 w-full mt-4 form">
+        
         <Button type="submit">Submit</Button>
       </form>
     </Form>
+    </div>
+    </div>
     </div>
   )
 }
---
+++ components/FormField.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const FormField = () => {
+  return (
+    <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+  )
+}
+
+export default FormField
